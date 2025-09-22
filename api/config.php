bc454895ca55de499634ae1406410b28aef5437f
MissterFreeze24 <romainmaufroy3@gmail.com>
2025-09-22 13:57:01 +0200

    Delete api directory

--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', getenv('MYSQL_HOST') ?: 'mysql');
-define('DB_NAME', getenv('MYSQL_DATABASE') ?: 'ecoride');
-define('DB_USER', getenv('MYSQL_USER') ?: 'ecoride_user');
-define('DB_PASS', getenv('MYSQL_PASSWORD') ?: 'ecoride_pass');
-define('AUTH_SECRET', getenv('AUTH_SECRET') ?: 'change_this_secret');
-?>
